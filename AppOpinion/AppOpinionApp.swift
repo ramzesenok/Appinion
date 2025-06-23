e53658533c15804cf5f19500bc51061f79278cd1
Roman Mirzoyan <roman.mirzoyan@erstegroup.com>
2025-06-23 23:34:19 +0200

    Initial Commit

---
+++ AppOpinion/AppOpinionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppOpinionApp.swift
+//  AppOpinion
+//
+//  Created by Roman Mirzoyan on 23.06.25.
+//
+
+import SwiftUI
+
+@main
+struct AppOpinionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
